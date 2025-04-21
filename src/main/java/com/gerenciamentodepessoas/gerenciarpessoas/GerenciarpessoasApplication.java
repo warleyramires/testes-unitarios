3c7f899e18baa4725b934da1c7203738785086a9
Warley Ramires <warleyramiresgoncalves@gmail.com>
2025-04-21 18:36:15 -0300

    feat: first commit

---
+++ src/main/java/com/gerenciamentodepessoas/gerenciarpessoas/GerenciarpessoasApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciamentodepessoas.gerenciarpessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciarpessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciarpessoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gerenciamentodepessoas/gerenciarpessoas/GerenciarpessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciamentodepessoas.gerenciarpessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciarpessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
